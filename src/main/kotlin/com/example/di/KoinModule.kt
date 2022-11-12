3c83e4c32c29a528cd6f681360e7624e34fc7353
bibekanandan892 <bibekanandan892@gmail.com>
2022-11-13 02:57:57 +0530

    test

--- src/main/kotlin/com/example/di/KoinModule.kt
+++ src/main/kotlin/com/example/di/KoinModule.kt
@@ -3,6 +3,7 @@ package com.example.di
 import com.example.data.UiDataSourceImpl
 import com.example.domain.repository.UiDataSource
 import com.example.utils.Constants.DATABASE_NAME
+import com.mongodb.ConnectionString
 import org.koin.dsl.module
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
@@ -11,7 +12,7 @@ import org.litote.kmongo.reactivestreams.KMongo
 val koinModule = module {
 
     single {
-        KMongo.createClient(System.getenv("MONGODB_URI"))
+        KMongo.createClient(ConnectionString("mongodb+srv://Bibek:4uzmr8HarQfklzcP@cluster0.2geeidr.mongodb.net/?retryWrites=true&w=majority"))
             .coroutine
             .getDatabase(DATABASE_NAME)
     }
